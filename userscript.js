d7624f942347bf3a5830495e2a09dbb7e877b688
Felix Yan <felixonmars@archlinux.org>
2022-10-23 21:38:14 +0300

    Match more contribution types

--- userscript.js
+++ userscript.js
@@ -8,13 +8,14 @@
 // @license MIT
 // ==/UserScript==
 
-$(".TimelineItem details details span[data-hovercard-type=repository]").each(function(index){
+$("details span[data-hovercard-type=repository], details a[data-hovercard-type=repository]").each(function(index){
     let item = this;
     let url = this.getAttribute('data-hovercard-url');
-    $.get(url, function(data){
-        let lang = data.match(/\"programmingLanguage\">([\w\s]+)</)[1];
-        let color = data.match(/background-color: (#\w+)\"/)[1];
-        $(item).append('<svg style="color:' + color + ';" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-dot-fill ml-2"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>');
-        $(item).append('<span class="color-fg-default text-bold f6">' + lang + '</span> ');
-    });
+    if(this.getAttribute("class") != "mr-2")
+        $.get(url, function(data){
+            let lang = data.match(/\"programmingLanguage\">(.+?)</)[1];
+            let color = data.match(/background-color: (#\w+)\"/)[1];
+            $(item).append('<svg style="color:' + color + ';" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-dot-fill ml-2"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>');
+            $(item).append('<span class="color-fg-default text-bold f6">' + lang + '</span> ');
+        });
 });
