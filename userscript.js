40dd0c6fed2a2807c4f99820f7f385cf25dbc686
Felix Yan <felixonmars@archlinux.org>
2026-05-19 22:18:07 +0300

    Migrate GitHub contribution language userscript
    Move github-show-contributions-language from its old standalone repository into userscripts collection.
    
    Fix it for current GitHub by scanning lazy-loaded contribution activity, fetching repository hovercards as XHR, resolving relative hovercard URLs before XMLHttpRequest, and inserting the language badge outside GitHub's truncated repository link text.

--- userscript.js
+++ userscript.js
@@ -1,21 +1,63 @@
 // ==UserScript==
 // @name                GitHub Show Contribution Languages
-// @namespace           https://github.com/felixonmars/github-show-contributions-language
-// @version             0.1
+// @namespace           https://github.com/felixonmars/userscripts
+// @version             0.2
 // @author              felixonmars
-// @require             https://cdn.jsdelivr.net/npm/jquery@3.6.0
 // @match               https://github.com/*
-// @license MIT
+// @homepageURL         https://github.com/felixonmars/userscripts
+// @supportURL          https://github.com/felixonmars/userscripts/issues
+// @updateURL           https://raw.githubusercontent.com/felixonmars/userscripts/main/github-show-contributions-language.user.js
+// @downloadURL         https://raw.githubusercontent.com/felixonmars/userscripts/main/github-show-contributions-language.user.js
+// @license             MIT
+// @grant               none
 // ==/UserScript==
 
-$("details span[data-hovercard-type=repository], details a[data-hovercard-type=repository]").each(function(index){
-    let item = this;
-    let url = this.getAttribute('data-hovercard-url');
-    if(this.getAttribute("class") != "mr-2")
-        $.get(url, function(data){
-            let lang = data.match(/\"programmingLanguage\">(.+?)</)[1];
-            let color = data.match(/background-color: (#\w+)\"/)[1];
-            $(item).append('<svg style="color:' + color + ';" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-dot-fill ml-2"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>');
-            $(item).append('<span class="color-fg-default text-bold f6">' + lang + '</span> ');
+(function () {
+    function scan() {
+        document.querySelectorAll('[data-hovercard-type=repository]:not([data-contribution-language])').forEach(function (item) {
+            let url = item.getAttribute('data-hovercard-url');
+
+            if (!url) {
+                return;
+            }
+
+            url = new URL(url, location.origin).href;
+
+            item.setAttribute('data-contribution-language', 'loading');
+
+            let request = new XMLHttpRequest();
+            request.open('GET', url);
+            request.setRequestHeader('Accept', 'text/html');
+            request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+            request.onload = function () {
+                if (request.status < 200 || request.status >= 300) {
+                    item.setAttribute('data-contribution-language', 'failed');
+                    return;
+                }
+
+                let data = request.responseText;
+                let lang = data.match(/itemprop="programmingLanguage">(.+?)</);
+                let color = data.match(/repo-language-color" style="background-color:\s*(#[0-9a-fA-F]+)"/);
+
+                if (!lang || !color) {
+                    item.setAttribute('data-contribution-language', 'missing');
+                    return;
+                }
+
+                item.insertAdjacentHTML('afterend', ' <span class="github-contribution-language no-wrap"><svg style="color:' + color[1] + ';" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-dot-fill ml-2"><path d="M8 4a4 4 0 1 1 0 8 4 4 0 0 1 0-8Z"></path></svg><span class="color-fg-default text-bold f6">' + lang[1] + '</span></span>');
+                item.setAttribute('data-contribution-language', 'done');
+            };
+            request.onerror = function () {
+                item.setAttribute('data-contribution-language', 'failed');
+            };
+            request.send();
         });
-});
+    }
+
+    scan();
+
+    new MutationObserver(scan).observe(document.documentElement, {
+        childList: true,
+        subtree: true
+    });
+})();
